f10041dd5d89a5493769da90f0cdc4b50d08b08b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-18 12:10:05 +0000

    Room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_18_115807) do
+ActiveRecord::Schema.define(version: 2023_07_18_120912) do
+
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "group_users", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +28,11 @@ ActiveRecord::Schema.define(version: 2023_07_18_115807) do
     t.integer "owner_id"
     t.string "name"
     t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
